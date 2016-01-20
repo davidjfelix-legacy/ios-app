27cd17babc63e2de2dfcfdc732670ddd84813901
Josh Newkirk <newkirk.josh@gmail.com>
2016-01-19 23:30:14 -0500

    Adding Google Sign-In and Analytics pods to the project

---
+++ Feasted/Feasted/Feasted-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Feasted-Bridging-Header.h
+//  Feasted
+//
+//  Created by Joshua Newkirk on 1/12/16.
+//  Copyright © 2016 Dinner Solutions LLC. All rights reserved.
+//
+
+#ifndef Feasted_Bridging_Header_h
+#define Feasted_Bridging_Header_h
+
+//#import <Google/SignIn.h>
+
+#endif /* Feasted_Bridging_Header_h */
