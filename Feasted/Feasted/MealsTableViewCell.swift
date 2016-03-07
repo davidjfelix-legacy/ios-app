86d7b59f44e4d786340ef44ef993454b8efe160d
Josh Newkirk <newkirk.josh@gmail.com>
2016-03-06 20:31:57 -0500

    Updated all outlets for mealstableviewcell

--- Feasted/Feasted/MealsTableViewCell.swift
+++ Feasted/Feasted/MealsTableViewCell.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class MealsTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
+class MealsTableViewCell: UITableViewCell
+{
+    // View Outlets
+    @IBOutlet weak var imageContainerView: UIView!
+    
+    // UILabels
+    @IBOutlet weak var areaCodeLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
+    @IBOutlet weak var mealTitleLabel: UILabel!
+    @IBOutlet weak var pickupTimeLabel: UILabel!
+    
+    // ImageViews
+    @IBOutlet weak var areaCodeImageView: UIImageView!
+    @IBOutlet weak var costImageView: UIImageView!
+    @IBOutlet weak var glutenFreeImageView: UIImageView!
+    @IBOutlet weak var mealImageView: UIImageView!
+    @IBOutlet weak var pickupTimeImageView: UIImageView!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var savedImageView: UIImageView!
+    
+    
+    override func awakeFromNib()
+    {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override func setSelected(selected: Bool, animated: Bool)
+    {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
     
