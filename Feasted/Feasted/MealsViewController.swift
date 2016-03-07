0187827d74d4ed3e9224e324776169ddfe605e72
Josh Newkirk <newkirk.josh@gmail.com>
2016-03-06 20:51:28 -0500

    Setting up meals view controller to use new view controller

--- Feasted/Feasted/MealsViewController.swift
+++ Feasted/Feasted/MealsViewController.swift
@@ -6,21 +6,39 @@
 //  Copyright © 2016 Dinner Solutions LLC. All rights reserved.
 //
 
+import UIKit
 import Foundation
-
 import Google
 
-class MealsViewController: GAITrackedViewController, UITableViewDataSource, UITableViewDelegate
+class MealsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    // View Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    // Data
     private var dataSource : Array<Meal>?
+    
+    private let cellIdentifier : String = "MealsTableViewCell"
+    
+    override func viewDidLoad()
+    {
+        self.dataSource = Array<Meal>()
+        self.tableView.registerNib(UINib.init(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
-        return UITableViewCell()
+        var cell : MealsTableViewCell? = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as? MealsTableViewCell
+        
+        if cell == nil {
+            cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as? MealsTableViewCell
+        }
+        
+        return cell!
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return dataSource!.count;
+        return dataSource!.count
     }
 }
