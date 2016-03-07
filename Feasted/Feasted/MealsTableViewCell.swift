6da96889f9e28b9dc96a44470c069f32dac8edc6
Josh Newkirk <newkirk.josh@gmail.com>
2016-03-06 20:26:36 -0500

    Adding MealsTableViewCell to project

---
+++ Feasted/Feasted/MealsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealsTableViewCell.swift
+//  Feasted
+//
+//  Created by Joshua Newkirk on 3/6/16.
+//  Copyright © 2016 Dinner Solutions LLC. All rights reserved.
+//
+
+import UIKit
+
+class MealsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
